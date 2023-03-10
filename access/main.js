5b12bf09a4e8275d15e76117684f62dc97a8cad4
thien0709 <thien070904@gmail.com>
2023-03-10 19:45:34 +0700

    add api

--- access/main.js
+++ access/main.js
@@ -1,7 +1,22 @@
+//Select 
+const input = document.querySelector('input');
+const temp = document.querySelector('.number');
+value = input.value;
+//Call API
 const API_KEY = "cf07c0b75fe0d374f648ef9cc52fb730";
-fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=Ho+Chi+Minh&appid=${API_KEY}`),
+const city = value;
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=vi`,
 {
-    headers:{
-        "Content-Type:"
+    headers: {
+      "Content-Type": "application/json",
+    },
+})
+.then((response) => response.json())
+.then((data) => console.log(data));
+window.onkeypress = (e){
+    if(e.key == "enter"){
+        console.log(city);
     }
 }
+// temp.innerText(data);
