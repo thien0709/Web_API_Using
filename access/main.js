79113c5b041ed1b7553180f4e58948ac5598ba98
thien0709 <thien070904@gmail.com>
2023-03-10 19:10:05 +0700

    first commit

---
+++ access/main.js
@@ -0,0 +1,7 @@
+const API_KEY = "cf07c0b75fe0d374f648ef9cc52fb730";
+fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=Ho+Chi+Minh&appid=${API_KEY}`),
+{
+    headers:{
+        "Content-Type:"
+    }
+}
