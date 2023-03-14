d3ba4039fe3f74aee2eaf6316ee6dec5f8f7e5fe
thien0709 <thien070904@gmail.com>
2023-03-14 10:25:08 +0700

    update js

--- access/main.js
+++ access/main.js
@@ -1,21 +1,21 @@
 //Select 
 const inputGet = document.querySelector('#inputGet');
 const temp = document.querySelector('.number');
-let cityName;
 let b;
 //Get value when enter
 window.onkeypress = function (event){
     if(event.keyCode == 13){
-        //Get value input
-        cityName = inputGet.value;
         // Call API
-        const API_KEY =`041b66d978aaa9fdcd80e2b063842130`;
-        var url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=041b66d978aaa9fdcd80e2b063842130&units=metric&lang=vi";
+        const API_KEY =`22e87aefe5b358c5e45fd150c7765eb6`;
+        var url = 'https://api.openweathermap.org/data/2.5/weather?q='+inputGet.value+'&appid=22e87aefe5b358c5e45fd150c7765eb6&units=metric&lang=vi';
         fetch(url)
         .then((response) => response.json())
-        .then((data) => console.log(data));
-        b = data;
-        console.log (b);
-
+        .then((data) =>{
+            var temperature = data['main']['temp'];
+            temp.innerHTML = temperature;
+        })
+        .catch((err) => {
+            alert("Hay nhap lai dia chi");
+        })
     }
 }
